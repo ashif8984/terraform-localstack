901fa2f38cc8a033fd407d05647912319b63f15d
Ashif Eqbal <ashif8984@gmail.com>
2026-08-19 23:26:30 +0530

    Added a new resource - networking bucket

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -6,3 +6,12 @@ module "application_bucket" {
   enable_logging = var.enable_logging
   project_name   = var.project_name
 }
+
+
+module "networking_bucket" {
+  source         = "../../modules/s3"
+  bucket_name    = "${var.project_name}-${var.environment}-network"
+  environment    = var.environment
+  enable_logging = var.enable_logging
+  project_name   = var.project_name
+}
