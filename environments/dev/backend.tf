596bb55227691e987156644c657353863510e6c8
Ashif Eqbal <ashif8984@gmail.com>
2026-08-19 23:13:08 +0530

    Formated backend.tf

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "s3" {
-    bucket         = "s3-state-bucket"
-    key            = "environments/dev/terraform.tfstate" # Different key per environment
-    region         = "us-east-1"
-    dynamodb_table = "terraform-locks"
-    # use_lockfile   = true
-    encrypt        = true
+    bucket = "s3-state-bucket"
+    key    = "environments/dev/terraform.tfstate" # Different key per environment
+    region = "us-east-1"
+    # dynamodb_table = "terraform-locks"
+    use_lockfile = true
+    encrypt      = true
   }
 }
